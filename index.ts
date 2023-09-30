fc565fd31fa1157163594ab1de06bd2d59005c2d
emmanueil1 <emmanueiljames@gmail.com>
2023-09-30 22:53:57 +0500

    Function and Return Function

---
+++ index.ts
@@ -0,0 +1,15 @@
+// function sum (num1:number, num2:number)
+// {
+//  var sum=num1+num2;
+//  console.log("sum =",sum);
+    
+// }
+// sum(8,9)
+function sum (num1:number, num2:number)
+{
+ var sum=(num1+num2);
+ //console.log("Marks =",sum);
+ return sum
+}
+var sum1=(sum(3265,3)+20);
+console.log("Total Marks =",sum1);
