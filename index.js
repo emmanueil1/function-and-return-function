460b2c2daabdc6e4fbce67e88c1834a0721383b9
emmanueil1 <emmanueiljames@gmail.com>
2023-10-02 00:53:09 +0500

    func and return func

--- index.js
+++ index.js
@@ -5,10 +5,13 @@
 //  console.log("sum =",sum);
 // }
 // sum(8,9)
-function sum(num1, num2) {
-    var sum = (num1 + num2);
-    //console.log("Marks =",sum);
-    return sum;
+function multiply1(num1, num2) {
+    var ans = (num1 * num2);
+    //console.log("Marks =",ans);
+    return ans;
 }
-var sum1 = (sum(3265, 3) + 20);
-console.log("Total Marks =", sum1);
+//var result=(multiply1(2,7)-1);
+//var result=(multiply1(2,7)-1.5);
+var result = (multiply1(2, 0) + 1);
+console.log("Marks =", result, "Marks =", multiply1(2, 1), "Marks =", multiply1(2, 2), "Marks =", multiply1(2, 3), "Marks =", multiply1(2, 4), "Marks =", multiply1(2, 5), "Marks =", multiply1(2, 6), "Marks =", multiply1(2, 7));
+//multiply1(2,1),multiply1(2,2),multiply1(2,3),multiply1(2,4),multiply1(2,5),multiply1(2,6),multiply1(2,7);
